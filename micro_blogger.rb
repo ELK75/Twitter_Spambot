90190b5c1f12ad3da7cc11ace57bbe34401b1e25
Elijah Kajinic <kajinic@yahoo.com>
2016-02-14 21:34:22 -0500

    finalized the haiku bot

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -56,32 +56,56 @@ class MicroBlogger
     end
   end
 
-  def clean_up_definition(definition)
-  	# gets rid of the denotion of whether or
-  	# not it's a noun/verb/adj/etc...
-  	definition.sub!(/[a-z]+\.\s/, '')
+  def syllable_count(sentance)
+    sentance.scan(/[aiouy]+e*|e(?!d$|ly).|[td]ed|le$/).size
   end
 
-  def tweet_word_and_definition(dictionary)
-  	while true
-  		phrases = %w(Wow! Cool! Neat! Rad! Groovy!)
-  		random_word = dictionary.keys.sample
-  		random_defintion = clean_up_definition(dictionary[random_word])
-  		random_phrase = phrases.sample
-  		word_of_day_tweet = "#{random_word} means #{random_defintion}\n#{random_phrase}"
-  		tweet(word_of_day_tweet)
-  		sleep(3600)
-  	end
+  def make_line(lines, syllables)
+    for index in 1...lines.size
+      syllables_in_line = syllable_count(lines[0..index].join(" "))
+      if syllables_in_line > syllables
+        return false
+      elsif syllables_in_line == syllables
+        haiku_line = lines[0..index]
+        lines.slice!(0, index+1)
+        return haiku_line
+      end
+    end
   end
 
-  def print_word_of_the_day
-    dictionary = Hash.new('')
-  	File.open("dictionary.txt").readlines.each do |line|
-  		word_and_def = line.split('  ')
-  		next if word_and_def.length != 2
-  		dictionary[word_and_def[0]] = word_and_def[1]
-  	end
-  	tweet_word_and_definition(dictionary)
+  def make_haiku(sentance)
+    return false if syllable_count(sentance) != 17
+    lines = sentance.split
+    begin
+      line_one =  make_line(lines, 5).join(" ")
+      line_two =  make_line(lines, 7).join(" ")
+      line_three = make_line(lines, 5).join(" ")
+      return "#{line_one}\n#{line_two}\n#{line_three}"
+    rescue NoMethodError
+      return false
+    end
+  end
+
+  def print_haiku
+    texts = ["The Catcher in the Rye",
+             "Fahrenheit 451", "To Kill a Mockingbird"]
+    chosen_text = texts[rand(0...texts.size)]
+    file = File.open("#{chosen_text}.txt")
+    contents = file.read.gsub!("\n", ' ')
+    sentances = contents.split(/(?:\.|\?|\!)(?= [^a-z]|$)/)
+    number_of_sentances = sentances.length
+    random_sentance = ""
+    while !make_haiku(random_sentance)
+      random_sentance = sentances[rand(0...number_of_sentances)]
+      random_sentance.gsub!(/\"/, '')
+      random_sentance.downcase!
+    end
+    haiku = "#{make_haiku(random_sentance)}\n\n"\
+            "Haiku from the book #{chosen_text}"
+    puts haiku
+    tweet(haiku)
+    sleep(3600)
+    print_haiku
   end
 
   def shorten(original_url)
@@ -113,6 +137,7 @@ class MicroBlogger
 				when 'turl' then tweet(parts[1..-2].join(" ") + 
 					" " + shorten(parts[-1]))
 				when 'wod' then print_word_of_the_day
+        when 'haiku' then print_haiku
 				else
 					puts "Sorry, I don't know how to #{command}"
 			end
---
+++ syl_test.rb
@@ -0,0 +1,12 @@
+
+s = "how are you doing this is a really long sentance phony"
+
+def new_count(word)
+  word.downcase!
+  return 1 if word.length <= 3
+  word.sub!(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '')
+  word.sub!(/^y/, '')
+  word.scan(/[aeiouy]{1,2}/).size
+end
+
+puts new_count(s)
